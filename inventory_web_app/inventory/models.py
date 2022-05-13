8ae2b2ec1664b5990862f030f93add96a3861cbf
Christopher Zhang <christopher.zhang@doctorcare.ca>
2022-05-13 15:46:53 -0400

    Create Models
        Add a FK relation of Product to Shipment. Also add a quantity field on a product to specify the amount of the product in the inventory.

--- inventory_web_app/inventory/models.py
+++ inventory_web_app/inventory/models.py
@@ -4,8 +4,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255, null=False)
-    sku = models.IntegerField(max_length=12, unique=True)
+    sku = models.CharField(max_length=12, unique=True)
     description = models.CharField(max_length=255, null=False)
+    quantity = models.IntegerField(default=0)
+    shipment = models.ForeignKey('Shipment', related_name='products', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'inventory_products'
