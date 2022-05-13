20d8876215b4f7dfa5a97865ab8c74267bc2d911
Christopher Zhang <christopher.zhang@doctorcare.ca>
2022-05-13 15:20:38 -0400

    Create New Web App

--- inventory_web_app/inventory_web_app/settings.py
+++ inventory_web_app/inventory_web_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'web_app',
+    'inventory.apps.InventoryConfig',
 ]
 
 MIDDLEWARE = [
--- inventory_web_app/inventory_web_app/urls.py
+++ inventory_web_app/inventory_web_app/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inventory/', include('inventory.urls')),
+    path('', RedirectView.as_view(url='inventory/', permanent=True))
 ]
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
