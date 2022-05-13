0ed92152d8fc4b29d61187487067430ee5af4319
Christopher Zhang <christopher.zhang@doctorcare.ca>
2022-05-13 15:35:36 -0400

    Create Models
        Create the Product and Shipment models.

--- inventory_web_app/inventory/models.py
+++ inventory_web_app/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    sku = models.IntegerField(max_length=12, unique=True)
+    description = models.CharField(max_length=255, null=False)
+
+    class Meta:
+        db_table = 'inventory_products'
+
+
+class Shipment(models.Model):
+    name = models.CharField(max_length=255, null=False)
+
+    class Meta:
+        db_table = 'inventory_shipments'
